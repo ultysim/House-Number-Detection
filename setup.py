fb4535498073f45b4c3f16b0b0f57af78dee16c7
Simas Glinskis <ultysim@gmail.com>
2020-02-18 17:22:47 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='House-Number-Detection',
+    version='',
+    packages=['HND'],
+    url='',
+    license='',
+    author='Simas',
+    author_email='',
+    description=''
+)
